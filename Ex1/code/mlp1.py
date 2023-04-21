f38dced40d93bbcc25bedee721f301e3b51d0a86
opalpeltzman <opalpeltzman@gmail.com>
2023-04-21 23:34:06 +0300

    implement train classifier and compute loss + gradients for ML perceptron

--- Ex1/code/mlp1.py
+++ Ex1/code/mlp1.py
@@ -1,17 +1,25 @@
 import numpy as np
+import loglinear as ll
+import math
 
-STUDENT={'name': 'YOUR NAME',
+STUDENT={'name': 'Opal Peltzman',
          'ID': 'YOUR ID NUMBER'}
+
 
 def classifier_output(x, params):
     # YOUR CODE HERE.
+    W, b = params[0], params[1]
+    h_l = np.tanh(np.dot(x, W) + b)
+    probs = ll.classifier_output(h_l, [params[2], params[3]])
     return probs
+
 
 def predict(x, params):
     """
     params: a list of the form [W, b, U, b_tag]
     """
     return np.argmax(classifier_output(x, params))
+
 
 def loss_and_gradients(x, y, params):
     """
@@ -27,7 +35,22 @@ def loss_and_gradients(x, y, params):
     gb_tag: vector, gradients of b_tag
     """
     # YOU CODE HERE
-    return ...
+    W, b, U, b_tag = params
+
+    y_hat = classifier_output(x=x, params=params)
+    # y_vec equals 1 in the correct label position (y)
+    y_vec = np.zeros(U.shape[1])
+    y_vec[y] = 1
+
+    loss = -math.log(y_hat[y])
+    z1 = np.tanh(x.dot(W) + b)   # output layer 1 (hidden layer)
+    gb_tag = y_hat - y_vec
+    gU = np.array([z1]).transpose().dot(np.array([gb_tag]))
+    gb = gb_tag.dot(U.transpose()) * (1 - np.power(z1, 2))
+    gW = np.array([x]).transpose().dot(np.array([gb]))
+
+    return loss, [gW, gb, gU, gb_tag]
+
 
 def create_classifier(in_dim, hid_dim, out_dim):
     """
@@ -38,6 +61,8 @@ def create_classifier(in_dim, hid_dim, out_dim):
     return:
     a flat list of 4 elements, W, b, U, b_tag.
     """
-    params = []
+
+
+    params = [W, b, U, b_tag]
     return params
 
---
+++ Ex1/code/train_mlp1.py
@@ -0,0 +1,155 @@
+import os
+import mlp1 as ml
+import random
+import numpy as np
+from math import log
+
+STUDENT = {'name': 'Opal Peltzman',
+           'ID': 'YOUR ID NUMBER'}
+
+
+def feats_to_vec(features):
+    # YOUR CODE HERE.
+    # Should return a numpy vector of features.
+    return np.array(features)
+
+
+def accuracy_on_dataset(dataset, params):
+    good = bad = 0.0
+    for label, features in dataset:
+        if ml.predict(x=features, params=params) == label:
+            good += 1
+        else:
+            bad += 1
+    return good / (good + bad)
+
+
+def train_classifier(train_data, dev_data, num_iterations, learning_rate, params):
+    """
+    Create and train a classifier, and return the parameters.
+
+    train_data: a list of (label, feature) pairs.
+    dev_data  : a list of (label, feature) pairs.
+    num_iterations: the maximal number of training iterations.
+    learning_rate: the learning rate to use.
+    params: list of parameters (initial values)
+    """
+    for I in range(num_iterations):
+        cum_loss = 0.0  # total loss in this iteration.
+        random.shuffle(train_data)
+        for label, features in train_data:
+            x = feats_to_vec(features)  # convert features to a vector.
+            y = label  # convert the label to number if needed.
+            loss, grads = ml.loss_and_gradients(x, y, params)
+            cum_loss += loss
+            # YOUR CODE HERE
+            # update the parameters according to the gradients
+            # and the learning rate.
+            params[0][0] -= grads[0][0] * learning_rate
+            params[0][1] -= grads[0][1] * learning_rate
+            params[1][0] -= grads[1][0] * learning_rate
+            params[1][1] -= grads[1][1] * learning_rate
+
+        train_loss = cum_loss / len(train_data)
+        train_accuracy = accuracy_on_dataset(train_data, params)
+        dev_accuracy = accuracy_on_dataset(dev_data, params)
+        print(I, train_loss, train_accuracy, dev_accuracy)
+    return params
+
+
+def set_path(name: str) -> str:
+    """
+    this function creates absolute path for a given file or folder
+    :param name - file or folder name
+    """
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', f'{name}'))
+    return path
+
+
+def read_data(folder: str, fname: str) -> list:
+    """
+        this function extracts file data
+        :param folder - folder name in order to set the path
+        :param fname - file name to read
+    """
+    data = []
+    path = set_path(folder)
+    file_name = os.path.join(path, fname)
+    with open(file_name, encoding='utf8') as f:
+        file_data = f.readlines()
+
+    for line in file_data:
+        label, text = line.strip().lower().split("\t", 1)
+        data.append((label, text))
+    return data
+
+
+def text_to_bigrams(text):
+    return ["%s%s" % (c1, c2) for c1, c2 in zip(text, text[1:])]
+
+
+def create_train_data(train_data: list) -> tuple:
+    """
+        this function creates language clusters dictionary and
+        biagram features dictionary
+        :param train_data - train data from train file provided
+    """
+    lan_classes = {lan: inx for inx, lan in enumerate(set(map(lambda x: x[0], train_data)))}
+    bigram_lists = [bigram for _, bigram in train_data]
+    flat_bigram_set = set([item for sublist in bigram_lists for item in sublist])
+    bigrams_features = {bigram: inx for inx, bigram in enumerate(flat_bigram_set)}
+
+    return lan_classes, bigrams_features
+
+
+def extract_info_from_data(data: list, lan_classes: dict, bigrams_features: dict) -> list:
+    """
+      this function creates a list that indicates the features for each language that
+      accrues in the given data list
+    """
+    info = []
+    for [language, biagram] in data:
+        lan_features = np.zeros(len(bigrams_features))
+        for b in biagram:
+            if b in bigrams_features:
+                lan_features[bigrams_features[b]] = 1
+        language_dict = lan_classes[language] if language in lan_classes else -1
+        info.append([language_dict, lan_features])
+    return info
+
+
+def predicting_test_data(lan_classes: dict, trained_params, output_file_name: str, test_data: list):
+    inv_lan_classes = {v: k for k, v in lan_classes.items()}
+    pred = [inv_lan_classes[ml.predict(x=data, params=trained_params)] for _, data in test_data]
+
+    with open(output_file_name, 'w') as f:
+        f.write('\n'.join(pred))
+
+
+if __name__ == '__main__':
+    # YOUR CODE HERE
+    # write code to load the train and dev sets, set up whatever you need,
+    # and call train_classifier.
+
+    # ...
+    learning_rate = 0.01
+    num_iterations = 10
+
+    TRAIN = [(l, text_to_bigrams(t)) for l, t in read_data(folder='data', fname='train')]
+    DEV = [(l, text_to_bigrams(t)) for l, t in read_data(folder='data', fname='dev')]
+    TEST = [(l, text_to_bigrams(t)) for l, t in read_data(folder='data', fname='test')]
+    _lan_classes, _bigrams_features = create_train_data(train_data=TRAIN)
+
+    train_data = extract_info_from_data(TRAIN, _lan_classes, _bigrams_features)
+    dev_data = extract_info_from_data(DEV, _lan_classes, _bigrams_features)
+    test_data = extract_info_from_data(TEST, _lan_classes, _bigrams_features)
+    in_dim = len(_bigrams_features)
+    hid_dim = int(log(len(_bigrams_features)))
+    out_dim = len(_lan_classes)
+
+    params = ml.create_classifier(in_dim, hid_dim, out_dim)
+    trained_params = train_classifier(train_data, dev_data, num_iterations, learning_rate, params)
+
+    predicting_test_data(lan_classes=_lan_classes, trained_params=trained_params,
+                         output_file_name='test.pred', test_data=test_data)
+
